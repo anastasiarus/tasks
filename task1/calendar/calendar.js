554e00182e6b72cc4549d5e4c3ebd21919c0d354
anastasiarus <81356013+anastasiarus@users.noreply.github.com>
2021-11-23 13:04:16 +0300

    Add files via upload

--- task1/calendar/calendar.js
+++ task1/calendar/calendar.js
@@ -2,16 +2,16 @@
     class Calendar {
 
       constructor(year,month){
-        this.month = month-1
+        this.month = month
         this.year = year
-        this.date = new Date(year,month)
+        this.date = new Date(year, month)
       }
 
       getDate() {
         
         let namesOfMonth = ['Январь','Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
-        let month = namesOfMonth[this.month]
-        return month + ' ' +this.year
+        let month = namesOfMonth
+        return month + this.year
         }
 
       getDaysOfWeek(date) {
@@ -63,47 +63,5 @@
     const addYear = prompt('Введите текущий год')
     const calendar = new Calendar(addMonth,addYear)
     addCalendar.innerHTML = calendar.createCalendar()
-  
-  /* const month = prompt('Введите номер текущего месяца')
-const year = prompt('Введите текущий год')
 
-function createCalendar(elem, year, month) {
-
-    let mon = month - 1
-    let d = new Date(year, mon)
-
-    let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>'
-
-    for (let i = 0; i < getDay(d); i++) {
-      table += '<td></td>'
-    }
-
-    while (d.getMonth() == mon) {
-      table += '<td>' + d.getDate() + '</td>';
-
-      if (getDay(d) % 7 == 6) { 
-        table += '</tr><tr>'
-      }
-
-      d.setDate(d.getDate() + 1)
-    }
-
-    if (getDay(d) != 0) {
-      for (let i = getDay(d); i < 7; i++) {
-        table += '<td></td>'
-      }
-    }
-
-    table += '</tr></table>'
-
-    elem.innerHTML = table
-  }
-
-  function getDay(date) { 
-    let day = date.getDay()
-    if (day == 0) day = 7
-    return day - 1
-  }
-
-  createCalendar(calendar, year, month) */
     
