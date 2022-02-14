b0c530ad6902ceae373c31e5133b10c98a355c01
Anastasia Rusak <81356013+anastasiarus@users.noreply.github.com>
2022-02-14 14:03:48 +0300

    Delete task1/buttons directory

--- task1/buttons/main.js
+++
@@ -1,9 +0,0 @@
-
-let count=0
-let btns = document.querySelectorAll('button')
-btns.forEach(function(btn) {
-  btn.addEventListener('click', function(e) {
-    count +=1
-    btn.innerHTML = count
-  })
-})
